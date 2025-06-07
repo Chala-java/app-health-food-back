92ceeeea56cd27432fc3aaaeb62fff3170a6999b
Chala-java <chalajuandavid876@gmail.com>
2025-06-07 18:54:24 -0500

    Deploy backend 2

--- src/main/java/com/example/PedidosApp/config/WebConfig.java
+++ src/main/java/com/example/PedidosApp/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 WebMvcConfigurer.super.addCorsMappings(registry);
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins("http://localhost:5173/" ,"http://localhost:5175/", "http://localhost:5175/"  )
                         .allowedMethods("GET","POST","PUT", "DELETE")
                         .allowedHeaders("*");
             }
